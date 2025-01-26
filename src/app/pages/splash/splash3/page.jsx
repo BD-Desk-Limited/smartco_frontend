e207a44be672a2d5820e28acf83f5afd1b4ad0b1
samson <samson@amatip.co.uk>
2025-01-26 08:22:09 +0000

    fixed logout error

--- src/app/pages/splash/splash3/page.jsx
+++ src/app/pages/splash/splash3/page.jsx
@@ -1,6 +1,6 @@
 'use client';
 import React from 'react';
-import Welcome from '@/components/auth/welcome/Welcome';
+import Welcome from '@/components/auth/Welcome'
 
 export default function page() {
 
