32f0401f85d3bd95df51e00f1384640f112332ef
samson <samson@amatip.co.uk>
2025-01-27 11:23:57 +0000

    added authorize device

--- src/services/authServices.jsx
+++ src/services/authServices.jsx
@@ -79,3 +79,31 @@ export const verifyOTPService = async (body) => {
     return { error: 'error verifying OTP, please try again' };
   }
 };
+
+
+export const authorizeDeviceService = async (body) => {
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/auth/add-trusted-device`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+      }
+    );
+
+    if (response.ok) {
+      const responseData = await response.json();
+      return { data: responseData };
+    } else {
+      const errorData = await response.json();
+      const errorMessage = errorData.message;
+      return { error: errorMessage };
+    }
+  } catch (error) {
+    console.error('Error:', error);
+    return { error: 'error authorizing device, please try again' };
+  }
+};
