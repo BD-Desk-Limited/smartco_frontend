a20dd28c102b4107a49f64bcaad33049cceb6aa4
samson <samson@amatip.co.uk>
2025-05-19 00:22:20 +0100

    ok

--- src/app/pages/auth/logout-all-devices/page.jsx
+++ src/app/pages/auth/logout-all-devices/page.jsx
@@ -4,13 +4,15 @@ const page = () => {
   return (
     <div>
         This page is for logging out all devices.
-        //take the token from url
-        //call the api to log out all devices
-        //show a success message
-        //redirect to login page
-        //show error message if token is invalid or expired
     </div>
   )
 }
 
 export default page;
+
+
+        //take the token from url
+        //call the api to log out all devices
+        //show a success message
+        //redirect to login page
+        //show error message if token is invalid or expired
